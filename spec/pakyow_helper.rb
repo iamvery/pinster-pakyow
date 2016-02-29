0d054257a02e69b433e329f954eec61e3977f71b
Jay Hayes <ur@iamvery.com>
2016-02-29 11:15:53 -0500

    Configure capybara for pakyow
    Unfortunately there is a bug in a pakyow test helper. The ugly monkey
    patch below fixes that. Hopefully it will be addressed by the lib
    :soon:.

--- spec/pakyow_helper.rb
+++ spec/pakyow_helper.rb
@@ -7,6 +7,20 @@ Dotenv.load(".env.test")
 
 Pakyow::TestHelp.setup
 
+Capybara.app = Pakyow.app
+
 RSpec.configure do |config|
   config.include Pakyow::TestHelp::Helpers
+end
+
+# Monkeypatch to fix issue in test helper
+# see https://gitter.im/pakyow/chat?at=56cded078bbe6265248f8188
+module Pakyow
+  class Request
+    def params
+      @params ||= Hash.strhash(super.merge!(env['pakyow.data'] || {}).merge!(JSON.parse(body.read.to_s)))
+    rescue JSON::JSONError
+      @params = Hash.strhash(super)
+    end
+  end
 end
