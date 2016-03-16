d718391ad095082ec992365f0ede3b984d634c55
Jay Hayes <ur@iamvery.com>
2016-03-16 17:31:31 -0500

    Bind empty data to form
    This ensures the form has been updated with appropriate attributes to
    handle form submission. e.g. The data-prop url will now have the input
    name `link[url]` as a result.

--- app/lib/routes.rb
+++ app/lib/routes.rb
@@ -8,6 +8,12 @@ Pakyow::App.routes do
       links = rom.relation(:links).to_a
 
       view.partial(:list).scope(:link).apply(links)
+
+      # TODO this in effect adds attributes to the form in the view for
+      # submitted params. See https://pakyow.org/docs/forms
+      # TODO rather than binding a hash that fills out the needed properties
+      # use a new, empty model.
+      view.partial(:form).scope(:link).bind(url: "")
     end
   end
 end
