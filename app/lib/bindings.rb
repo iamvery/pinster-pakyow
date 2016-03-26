91a5fcbc437d1c7a34269d902a45bc6aa39f2210
Jay Hayes <ur@iamvery.com>
2016-03-26 11:04:02 -0500

    Add note about updating binding

--- app/lib/bindings.rb
+++ app/lib/bindings.rb
@@ -2,6 +2,7 @@ Pakyow::App.bindings do
   scope :link do
     restful :link
 
+    # TODO https://github.com/iamvery/pinster-pakyow/pull/7#issuecomment-197944275
     binding :url do
       {
         href: bindable[:url],
